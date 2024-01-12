f333c1637faec75e1f4968351cd96c7e183f7fab
beemaster1972 <ugor.finno@yandex.ru>
2024-01-12 13:09:28 +0500

    Final solution of home work 3

--- homeWork3/script/task1.js
+++ homeWork3/script/task1.js
@@ -4,19 +4,12 @@
 ввод вашего любимого фильма и если ввод неверен, 
 то программа отправляет запрос alert “Попробуйте ещё раз”.
 */
-// let task1 = document.getElementById("task1")
-// console.log(task1)
-// let elements = task1.getElementsByClassName("movie")
-// console.log(elements)
-// let my_fav_movie = [].map.call(elements, element => element.textContent)
-// console.log(my_fav_movie)
-var movieFav = document.getElementById("task1").textContent
-console.log(movieFav)
+var movieFav = document.getElementById("task1").textContent.trim()
 let user_answer = prompt("Угадайте мой любимый фильм")
 if (user_answer.toLocaleLowerCase() === movieFav.toLocaleLowerCase()) alert("Да ты угада!!!")
 else {
-     document.write(user_answer)
+     document.write("["+user_answer+"]")
      document.write("<br>")
-     document.write(movieFav)
+     document.write("["+movieFav+"]")
      alert("Попробуйте ещё раз")
 }
